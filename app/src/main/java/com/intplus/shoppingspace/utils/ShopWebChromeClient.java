152962fbdf7baa43dc586178c85308235fa1d397
Shree Harsha <harshas@vmware.com>
2017-03-18 13:24:55 +0530

    code changes

---
+++ app/src/main/java/com/intplus/shoppingspace/utils/ShopWebChromeClient.java
@@ -0,0 +1,12 @@
+package com.intplus.shoppingspace.utils;
+
+import android.webkit.WebChromeClient;
+
+/**
+ * Created by harshas on 3/11/2017.
+ */
+
+public class ShopWebChromeClient extends WebChromeClient {
+
+
+}
